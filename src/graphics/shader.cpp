bc452d237ecd951447c3289de79eaac3f55aa58e
cstdout <vkhrustalev42@gmail.com>
2026-02-15 18:21:36 +0400

    Implement Shader::loadCodeFromFiles()

--- src/graphics/shader.cpp
+++ src/graphics/shader.cpp
@@ -10,3 +10,33 @@
 Shader::Shader()
 {
 }
+bool Shader::loadCodeFromFiles(const std::string &vertexFile, const std::string &fragmentFile)
+{
+    std::ifstream vShaderFile;
+    std::ifstream fShaderFile;
+
+    vShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+    fShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+    try {
+        vShaderFile.open(vertexFile);
+        fShaderFile.open(fragmentFile);
+        std::stringstream vShaderStream, fShaderStream;
+
+        vShaderStream << vShaderFile.rdbuf();
+        fShaderStream << fShaderFile.rdbuf();
+
+        vShaderFile.close();
+        fShaderFile.close();
+
+        _vShaderCode.clear();
+        _fShaderCode.clear();
+
+        _vShaderCode = vShaderStream.str();
+        _fShaderCode = fShaderStream.str();
+    }
+    catch(std::ifstream::failure& e) {
+            std::cerr << "ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ" << std::endl;
+            return false;
+    }
+    return true;
+}
