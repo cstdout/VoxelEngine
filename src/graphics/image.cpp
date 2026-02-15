539c2f601c292bd36b43bd0fa2ff21fff7c9469a
cstdout <vkhrustalev42@gmail.com>
2026-02-15 17:45:16 +0400

    Add headers, implement constructors

---
+++ src/graphics/image.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include "image.h"
+#define STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image.h"
+#include "stb_image_write.h"
+
+Image::Image()
+{
+}
+Image::Image(const Image& other)
+{
+    bytes = other.bytes;
+    _width = other._width;
+    _height = other._height;
+    _channels = other._channels;
+    _path = other._path;
+
+    _size = other._size;
+}
+Image::Image(uint32_t width, uint32_t height, uint32_t channels)
+{
+    _width = width;
+    _height = height;
+    _channels = channels;
+    uint32_t s = width * height * channels;
+    _size = s;
+    bytes = new uint8_t[s];
+    std::fill_n(bytes, s, 0);
+}
