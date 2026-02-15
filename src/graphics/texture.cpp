1de05762129e8cf3e28b793b6b95384245caa28a
cstdout <vkhrustalev42@gmail.com>
2026-02-15 17:59:05 +0400

    Add headers, implement constructors

---
+++ src/graphics/texture.cpp
@@ -0,0 +1,22 @@
+#include "texture.h"
+#include <iostream>
+Texture::Texture(uint32_t id, int32_t width, int32_t height, int32_t channels) : _id(id), _width(width), _height(height), _channels(channels)
+{
+}
+Texture::Texture()
+{
+    _id = 0;
+    _width = 0;
+    _height = 0;
+    _channels = 0;
+    _activeSlot = 0;
+}
+Texture::Texture(const Image* image)
+{
+    _image = image;
+}
+Texture::Texture(const Image* image, const TextureConfig& config)
+{
+    _image = image;
+    _config = config;
+}
