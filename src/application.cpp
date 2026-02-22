763d759612bdfa5167629c81577f160894b8be7b
cstdout <vkhrustalev42@gmail.com>
2026-02-22 12:10:27 +0400

    Init construcotr

--- src/application.cpp
+++ src/application.cpp
@@ -1,6 +1,12 @@
+#include <iostream>
 #include "application.h"
+#include "window.h"
 
+bool Application::glfwInitialized = false;
 Application::Application()
 {
-
+    if (!(Application::glfwInitialized) && glfwInit() == GLFW_TRUE)
+    {
+        Application::glfwInitialized = true;
+    }
 }
