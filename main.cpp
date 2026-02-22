065c3b9466a3086cefd08fffff51397e8813f2d5
cstdout <vkhrustalev42@gmail.com>
2026-02-22 13:26:00 +0400

    Connect Window, Application and Renderer instances into one file

--- main.cpp
+++ main.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
-
+#include "src/application.h"
+#include "src/window.h"
+#include "src/graphics/renderer/renderer.h"
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+
+    const int32_t WINDOW_WIDTH = 720;
+    const int32_t WINDOW_HEIGHT = 680;
+
+    Renderer renderer;
+    Window w(WINDOW_WIDTH, WINDOW_HEIGHT, "My Window");
+
+    Application app;
+    app.setWindow(&w);
+    app.setRenderer(&renderer);
+
+    app.run();
+
+
     return 0;
 }
