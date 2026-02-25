0ec2b34bbaa5987d0c95ef02f21ef79896009342
cstdout <vkhrustalev42@gmail.com>
2026-02-25 21:19:09 +0400

    Implement MeshRenderer::initVertexBuffer()

--- src/graphics/renderer/meshrenderer.cpp
+++ src/graphics/renderer/meshrenderer.cpp
@@ -22,3 +22,15 @@ void MeshRenderer::setShader(Shader *shader)
 {
     _shader = shader;
 }
+void MeshRenderer::initVertexBuffer(int32_t vertexCoordInputLocation)
+{
+    glGenBuffers(1, &_vertexBuffer);
+
+    glBindBuffer(GL_ARRAY_BUFFER, _vertexBuffer);
+    glBufferData(GL_ARRAY_BUFFER, _vertexCount * sizeof(float), _mesh->vertices, GL_STATIC_DRAW);
+
+
+    glEnableVertexAttribArray(vertexCoordInputLocation);
+    glBindBuffer(GL_ARRAY_BUFFER, _vertexBuffer);
+    glVertexAttribPointer(vertexCoordInputLocation, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+}
