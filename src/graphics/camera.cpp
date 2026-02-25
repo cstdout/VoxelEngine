0a013098f9fa7c6e115eb00143461cb58ddcfbc0
cstdout <vkhrustalev42@gmail.com>
2026-02-25 21:12:48 +0400

    Implement Camera::init()

--- src/graphics/camera.cpp
+++ src/graphics/camera.cpp
@@ -4,3 +4,24 @@ Camera::Camera()
 {
 
 }
+void Camera::init(Mat4* viewMatrix,
+                  Vec3 position,
+                  float upX,
+                  float upY,
+                  float upZ,
+                  float yaw,
+                  float pitch,
+                  float movementSpeed,
+                  float mouseSensitivity)
+{
+    this->viewMatrix = viewMatrix;
+    Position = position;
+    WorldUp = Vec3(upX, upY, upZ);
+    Yaw = yaw;
+    Pitch = pitch;
+    updateCameraVectors();
+    Front = Vec3(0.0f, 0.0f, -1.0f);
+
+    Speed = movementSpeed;
+    Sensitivity = mouseSensitivity;
+}
