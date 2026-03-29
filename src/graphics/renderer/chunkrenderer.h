f7f5d47f2f8da5b2ea7f30682359d78ad817434b
cstdout <vkhrustalev42@gmail.com>
2026-03-30 03:57:36 +0400

    Add headers

--- src/graphics/renderer/chunkrenderer.h
+++ src/graphics/renderer/chunkrenderer.h
@@ -1,11 +1,26 @@
 #ifndef CHUNKRENDERER_H
 #define CHUNKRENDERER_H
 
-
-class ChunkRenderer
+#include "src/graphics/renderer/meshrenderer.h"
+#include "src/graphics/texture.h"
+class ChunkRenderer : public MeshRenderer
 {
 public:
-    ChunkRenderer();
+    ChunkRenderer(const Mesh* mesh, int32_t viewportWidth = 0, int32_t viewportHeight = 0);
+    ~ChunkRenderer();
+    void setTextureAtlas(Texture* atlas)
+    {
+        _textureAtlas = atlas;
+    }
+    void init();
+
+protected:
+    Texture* _textureAtlas = nullptr;
+    uint32_t _texCoordBuffer = 0;
+    void initTextureCoords(int32_t texCoordsBufferInputLocation = 1);
+    void linkUniforms();
+    void onDraw(float delta, int32_t w, int32_t h);
+
 };
 
 #endif // CHUNKRENDERER_H
