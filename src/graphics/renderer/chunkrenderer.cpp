3ddc01fc624a37d19a9dba4cda1176f6efb80a84
cstdout <vkhrustalev42@gmail.com>
2026-03-30 03:57:49 +0400

    Implement ~ChunkRenderer()

--- src/graphics/renderer/chunkrenderer.cpp
+++ src/graphics/renderer/chunkrenderer.cpp
@@ -4,3 +4,11 @@ ChunkRenderer::ChunkRenderer()
 {
 
 }
+ChunkRenderer::~ChunkRenderer()
+{
+    if(_texCoordBuffer)
+    {
+        glDeleteBuffers(1, &_texCoordBuffer);
+    }
+    _textureAtlas = nullptr;
+}
