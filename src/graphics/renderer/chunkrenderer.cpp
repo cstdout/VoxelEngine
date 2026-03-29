6241e0d73a48f6cc9e70efb22b5ba03b3b73d570
cstdout <vkhrustalev42@gmail.com>
2026-03-30 03:54:33 +0400

    Init ChunkRenderer class

---
+++ src/graphics/renderer/chunkrenderer.cpp
@@ -0,0 +1,6 @@
+#include "chunkrenderer.h"
+
+ChunkRenderer::ChunkRenderer()
+{
+
+}
---
+++ src/graphics/renderer/chunkrenderer.h
@@ -0,0 +1,11 @@
+#ifndef CHUNKRENDERER_H
+#define CHUNKRENDERER_H
+
+
+class ChunkRenderer
+{
+public:
+    ChunkRenderer();
+};
+
+#endif // CHUNKRENDERER_H
