f70c1303236d166c8be54480d25a4181b5c85d2b
cstdout <vkhrustalev42@gmail.com>
2026-03-30 04:22:03 +0400

    Init PerlinNoise::randomGradient()

--- src/vectormath/perlinnoise.cpp
+++ src/vectormath/perlinnoise.cpp
@@ -1,8 +1,27 @@
 #include "perlinnoise.h"
+#include <cmath>
 
 PerlinNoise::PerlinNoise(uint32_t noiseMapSize)
 {
     mapSize = noiseMapSize;
     noiseArrSize = mapSize * mapSize;
     noise = new float[noiseArrSize];
+}
+void PerlinNoise::randomGradient(int32_t x, int32_t y, float* outX, float* outY)
+{
+    const uint32_t W = 8 * sizeof(uint32_t);
+    const uint32_t S = (W >> 1);
+    uint32_t a = uint32_t(x);
+    uint32_t b = uint32_t(y);
+    a *= 3284157443;
+
+    b ^= a << S | a >> W - S;
+    b *= 1911520717;
+
+    a ^= b << S | b >> W - S;
+    a *= 2048419325;
+    float random = a * (3.14159265f / ~(~0u >> 1)); // in [0, 2*Pi]
+
+    (*outX) = sinf(random);
+    (*outY) = cosf(random);
 }
