75ab0b5cf95496dabc4d23fffb781c084f044b9b
cstdout <vkhrustalev42@gmail.com>
2026-03-30 04:02:16 +0400

    Implement ChunkRenderer::initTextureCoords()

--- src/graphics/renderer/chunkrenderer.cpp
+++ src/graphics/renderer/chunkrenderer.cpp
@@ -11,3 +11,16 @@ ChunkRenderer::~ChunkRenderer()
     }
     _textureAtlas = nullptr;
 }
+void ChunkRenderer::initTextureCoords(int32_t texCoordsBufferInputLocation)
+{
+
+    glGenBuffers(1, &_texCoordBuffer);
+
+    glBindBuffer(GL_ARRAY_BUFFER, _texCoordBuffer);
+    glBufferData(GL_ARRAY_BUFFER, _mesh->uvSize * sizeof(float), _mesh->uvs, GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(texCoordsBufferInputLocation);
+    glBindBuffer(GL_ARRAY_BUFFER, _texCoordBuffer);
+    glVertexAttribPointer(texCoordsBufferInputLocation, 2, GL_FLOAT, GL_FALSE, 0, (void*)0);
+
+}
