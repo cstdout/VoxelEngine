d23d2796f9b6941eff9d376bab764fcc479974cc
cstdout <vkhrustalev42@gmail.com>
2026-04-24 10:31:22 +0400

    Implement destructor

--- src/graphics/mesh/world/region.cpp
+++ src/graphics/mesh/world/region.cpp
@@ -2,7 +2,26 @@
 
 Region::Region()
 {
+    const float CHUNK_WIDTH = float(Chunk::WIDTH);
+    const float CHUNK_HEIGHT = float(Chunk::HEIGHT);
+    const float CHUNK_DEPTH = float(Chunk::DEPTH);
+    chunks = new Chunk**[WIDTH_IN_CHUNKS];
+    for(uint32_t i = 0; i < WIDTH_IN_CHUNKS; ++i)
+    {
+        chunks[i] = new Chunk*[DEPTH_IN_CHUNKS];
+        for(uint32_t k = 0; k < DEPTH_IN_CHUNKS; ++k)
+        {
+            chunks[i][k] = new Chunk[HEIGHT_IN_CHUNKS];
+            for(uint32_t j = 0; j < HEIGHT_IN_CHUNKS; ++j)
+            {
+                chunks[i][k][j].translate(float(i) * CHUNK_WIDTH,
+                                          float(j) * CHUNK_HEIGHT,
+                                          float(k) * CHUNK_DEPTH);
+            }
+        }
+    }
 
+    meshes = new Mesh*[getAreaInChunks()];
 }
 uint32_t Region::getTotalBlockCount()
 {
@@ -19,4 +38,17 @@ uint32_t Region::getAreaInBlocks()
 uint32_t Region::getAreaInChunks()
 {
     return WIDTH_IN_CHUNKS * DEPTH_IN_CHUNKS;
+}
+Region::~Region()
+{
+    for(uint32_t i = 0; i < WIDTH_IN_CHUNKS; ++i)
+    {
+        for(uint32_t k = 0; k < DEPTH_IN_CHUNKS; ++k)
+        {
+            delete [] chunks[i][k];
+        }
+        delete [] chunks[i];
+    }
+    delete [] chunks;
+    chunks = nullptr;
 }
